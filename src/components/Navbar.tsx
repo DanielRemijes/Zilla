9a8d98f76a55b9573c447023e28e114fe63e9695
DanielRemijes <danielremijes2002@gmail.com>
2026-09-03 20:46:43 +0530

    Logo Size Correction

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,7 @@ export default function Navbar() {
               width={168}
               height={76}
               priority
-              className="h-10 w-auto sm:h-14 md:h-12 lg:h-14 xl:h-16"
+              className="h-16 w-auto sm:h-16 md:h-16 lg:h-16 xl:h-16"
             />
           </Link>
 
